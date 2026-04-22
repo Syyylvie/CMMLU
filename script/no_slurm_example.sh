e7f4763d28355d2a15bf38e49277d7cf7d90198c
carry <842418433@qq.com>
2026-04-22 17:21:18 +0800

    Update Qwen3 results

--- script/no_slurm_example.sh
+++ script/no_slurm_example.sh
@@ -1,11 +1,11 @@
 set -x
 cd ../src
 
-python3 qwen3.py \
-    --model_name_or_path Qwen/Qwen3-1.7B \
-    --save_dir ../results/Qwen3-1.7B \
+python qwen1.5.py \
+    --model_name_or_path Qwen/Qwen1.5-72B \
+    --save_dir ../results/Qwen1.5-72B \
     --num_few_shot 5
-python3 qwen3.py \
-    --model_name_or_path Qwen/Qwen3-1.7B \
-    --save_dir ../results/Qwen3-1.7B \
+python qwen1.5.py \
+    --model_name_or_path Qwen/Qwen1.5-72B \
+    --save_dir ../results/Qwen1.5-72B \
     --num_few_shot 0
