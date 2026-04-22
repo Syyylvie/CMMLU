0c753ea29b140aee380f72a1e3d1c71c52f5cc32
carry <842418433@qq.com>
2026-04-22 17:16:16 +0800

    Reverse changes

--- script/no_slurm_example.sh
+++ script/no_slurm_example.sh
@@ -3,9 +3,9 @@ cd ../src
 
 python qwen1.5.py \
     --model_name_or_path Qwen/Qwen1.5-72B \
-    --save_dir ../results/Qwen3-1.7B \
+    --save_dir ../results/Qwen1.5-72B \
     --num_few_shot 5
-python3 qwen1.5.py \
+python qwen1.5.py \
     --model_name_or_path Qwen/Qwen1.5-72B \
-    --save_dir ../results/Qwen3-1.7B \
+    --save_dir ../results/Qwen1.5-72B \
     --num_few_shot 0
